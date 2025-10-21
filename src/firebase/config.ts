7e84bad0a201bec9310066e110a14a46ca1dba5d
Bayapuneni Investments <startup@biginess.com>
2025-10-21 13:53:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "bingotango",
+  "appId": "1:819473428171:web:b4153220777675bb86b54e",
+  "apiKey": "AIzaSyC4MsFEgnYA54bjMJ9xBSBAFgavCEORc-w",
+  "authDomain": "bingotango.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "819473428171"
+};
