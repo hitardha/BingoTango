33f4ded6a91ac1bfece12585827fef79165fe103
Bayapuneni Investments <startup@biginess.com>
2025-10-22 07:56:19 +0000

    use the follwing updated points: for 3x3
    Each Matched Number: 30 points

--- src/lib/score-calculator.ts
+++ src/lib/score-calculator.ts
@@ -2,24 +2,24 @@
 export const scoreWeights = {
     "3": {
         cell: 30,
-        line: 9,
+        line: 14,
         nMinus1: 6,
         nMinus2: 3,
         corners: 12,
     },
     "4": {
         cell: 40,
-        line: 16,
+        line: 26,
         nMinus1: 12,
         nMinus2: 8,
         corners: 20,
     },
     "5": {
         cell: 50,
-        line: 25,
+        line: 36,
         nMinus1: 15,
         nMinus2: 10,
-        corners: 36,
+        corners: 28,
     }
 };
 
